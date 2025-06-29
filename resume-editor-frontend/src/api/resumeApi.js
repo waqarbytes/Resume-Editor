fe7345cd1134ed0f33d0e8c59f59a79d6b6e2f4a
Mohammed Waqar <137026365+waqarbytes@users.noreply.github.com>
2025-06-29 23:12:46 +0530

    Update resumeApi.js

--- resume-editor-frontend/src/api/resumeApi.js
+++ resume-editor-frontend/src/api/resumeApi.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://resume-editor-gtvf.onrender.com";
 
 export async function uploadResume(file) {
   const formData = new FormData();
